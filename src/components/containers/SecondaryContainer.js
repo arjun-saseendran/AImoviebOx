6abb37491d24c746e908485d891a662d13bed533
arjun-saseendran <arjunsaseendran@icloud.com>
2025-04-25 21:20:37 +0530

    add SecondaryContainer, MovieCard, MovieList and VideoTitle

--- src/components/containers/SecondaryContainer.js
+++ src/components/containers/SecondaryContainer.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { MovieList } from "../MovieList/MovieList";
 
 export const SecondaryContainer = () => {
+  const movies = useSelector((store) => store?.movies);
+
   return (
-    <div>SecondaryContainer</div>
-  )
-}
+    movies.nowPlayingMovies && (
+      <div className=" bg-black">
+        <div className="-mt-44 relative z-20 pl-12">
+
+        <MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies} />
+        <MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies} />
+        <MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies} />
+      </div>
+        </div>
+    )
+  );
+};
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,10 @@
 export const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Y2Y3NzkzMmQ0ZTg2ZmFkMmI3MzdlMmYyODU1ZGY2NiIsIm5iZiI6MTcwNDU5OTUzOC45MTgwMDAyLCJzdWIiOiI2NTlhMWZmMjRkMGU4ZDAxZmQ5ZTRjNGIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.PDMwUw0QSJOxFAZji-16xJs3msewGfH1tm3nldgecqM'
-    }
-  };
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Y2Y3NzkzMmQ0ZTg2ZmFkMmI3MzdlMmYyODU1ZGY2NiIsIm5iZiI6MTcwNDU5OTUzOC45MTgwMDAyLCJzdWIiOiI2NTlhMWZmMjRkMGU4ZDAxZmQ5ZTRjNGIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.PDMwUw0QSJOxFAZji-16xJs3msewGfH1tm3nldgecqM",
+  },
+};
+
+export const IMG_CDN = "https://image.tmdb.org/t/p/w500";
